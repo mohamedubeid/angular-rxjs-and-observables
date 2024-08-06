852512899e9bc363014aecd72792eec8a1331ba9
Mohamed Ubeid <mubaid@vast.ps>
2024-08-06 13:53:02 +0300

    error and completion of abservable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,15 +14,25 @@ export class AppComponent{
   myObservable = new Observable((observer) => {
     // observer.next([1, 2, 3, 4, 5]);\
     setTimeout(() => {observer.next(1)}, 1000);
-    setTimeout(() => {observer.next(2)}, 3000);
-    setTimeout(() => {observer.next(3)}, 2000);
+    setTimeout(() => {observer.next(2)}, 2000);
+    setTimeout(() => {observer.next(3)}, 3000);
+    // setTimeout(() => {observer.error(new Error('there is an error here.'))}, 3000);
     setTimeout(() => {observer.next(4)}, 4000);
     setTimeout(() => {observer.next(5)}, 5000);
+    setTimeout(() => {observer.complete()}, 6000);
   })
 
   GetAsyncData() {
-    this.myObservable.subscribe((val: any) => {
-      this.data.push(val);
+    this.myObservable.subscribe({
+      next: (val: any) => {
+        this.data.push(val);
+      },
+      error: (err) => {
+        alert(err);
+      },
+      complete: () => {
+        alert('straming is done!')
+      }
     })
   }
 }
