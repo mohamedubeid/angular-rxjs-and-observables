21bf082d8f6eb8c283648a4d905ec1bd28d6eda3
Mohamed Ubeid <mubaid@vast.ps>
2024-08-06 14:22:04 +0300

    rxjs of() & from() operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of, from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,27 +11,35 @@ export class AppComponent{
 
   data: any[] = [];
 
-  myObservable = new Observable((observer) => {
-    // observer.next([1, 2, 3, 4, 5]);\
-    setTimeout(() => {observer.next(1)}, 1000);
-    setTimeout(() => {observer.next(2)}, 2000);
-    setTimeout(() => {observer.next(3)}, 3000);
-    // setTimeout(() => {observer.error(new Error('there is an error here.'))}, 3000);
-    setTimeout(() => {observer.next(4)}, 4000);
-    setTimeout(() => {observer.next(5)}, 5000);
-    setTimeout(() => {observer.complete()}, 6000);
-  })
+  // myObservable = new Observable((observer) => {
+  //   // observer.next([1, 2, 3, 4, 5]);\
+  //   setTimeout(() => {observer.next(1)}, 1000);
+  //   setTimeout(() => {observer.next(2)}, 2000);
+  //   setTimeout(() => {observer.next(3)}, 3000);
+  //   // setTimeout(() => {observer.error(new Error('there is an error here.'))}, 3000);
+  //   setTimeout(() => {observer.next(4)}, 4000);
+  //   setTimeout(() => {observer.next(5)}, 5000);
+  //   setTimeout(() => {observer.complete()}, 6000);
+  // })
+
+  array1 = [1, 2, 3 ,4 ,5, 6];
+  array2 = ['A', 'B', 'C'];
+  // myObservable = of(this.array1, this.array2, 2, 4, 5);
+  myObservable = from(this.array2);
+
 
   GetAsyncData() {
     this.myObservable.subscribe({
       next: (val: any) => {
         this.data.push(val);
+        console.log(val)
       },
       error: (err) => {
         alert(err);
       },
       complete: () => {
-        alert('straming is done!')
+        // alert('straming is done!')
+        console.log('completed!!')
       }
     })
   }
