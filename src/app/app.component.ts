db498c97774b40d75b3d4e225ab946a98d22ee89
MohamedUbeid9 <mubaid@vast.ps>
2024-08-06 11:52:32 +0300

    creating and using observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,19 @@ export class AppComponent{
   title = 'angular-observables';
 
   data: any[] = [];
+
+  myObservable = new Observable((observer) => {
+    // observer.next([1, 2, 3, 4, 5]);\
+    setTimeout(() => {observer.next(1)}, 1000);
+    setTimeout(() => {observer.next(2)}, 3000);
+    setTimeout(() => {observer.next(3)}, 2000);
+    setTimeout(() => {observer.next(4)}, 4000);
+    setTimeout(() => {observer.next(5)}, 5000);
+  })
+
+  GetAsyncData() {
+    this.myObservable.subscribe((val: any) => {
+      this.data.push(val);
+    })
+  }
 }
